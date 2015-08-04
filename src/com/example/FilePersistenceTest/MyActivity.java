de4b4e5108e2a9c39ca7a61aa2f412ef62e14267
ChinaDarren <chinadarren@outlook.com>
2015-08-05 00:14:22 +0800

    s

--- src/com/example/FilePersistenceTest/MyActivity.java
+++ src/com/example/FilePersistenceTest/MyActivity.java
@@ -4,13 +4,11 @@ import android.app.Activity;
 import android.content.Context;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class MyActivity extends Activity {
     /**
@@ -23,8 +21,41 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         edit = (EditText) findViewById(R.id.edit);
+        //调用load方法读取文件内容
+        String inputText = load();
+   //  TextUtils.isEmpty判断字符串等于 null 或者等于 空字符串 返回true，同时判断两个值
+        if(!TextUtils.isEmpty(inputText)){
+            edit.setText(inputText);
+            //setSelection设定光标位置
+            edit.setSelection(inputText.length());
+            Toast.makeText(this,"Restoring succedded",Toast.LENGTH_SHORT).show();
+        }
     }
-
+    public String load(){
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try{
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while((line = reader.readLine()) != null){
+                content.append(line);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(reader != null){
+                try{
+                    reader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
+    }
+//执行到onDestroy时调用save方法保存
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -37,6 +68,8 @@ public class MyActivity extends Activity {
         FileOutputStream out = null;
         BufferedWriter writer = null;
         try {
+         //创建文件data，类型private
+            //系统默认/data/data/<package name>/files/目录
             out = openFileOutput("data", Context.MODE_PRIVATE);
             writer = new BufferedWriter(new OutputStreamWriter(out));
             writer.write(inputText);
